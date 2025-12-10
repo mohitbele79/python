b63537888851666e1a995d82f4c92af929286e9e
Mohit Bele <mohitbele79@gmail.com>
2025-12-10 12:03:25 +0530

    hi

--- add.py
+++ add.py
@@ -19,3 +19,12 @@ class Person:
 p1 = Person("Emil", 25)
 print(p1.name)
 print(p1.__age) # This will cause an error
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age # Private property
+
+p1 = Person("Emil", 25)
+print(p1.name)
+print(p1.__age) # This will cause an error
